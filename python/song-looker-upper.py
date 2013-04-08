8600ea88fd62461f3b38171877c4b3a67f2874c5
Kevin Barnes <kbarnes3@gmail.com>
2013-04-07 21:54:26 -0700

    Hard-coded lookup works

--- python/song-looker-upper.py
+++ python/song-looker-upper.py
@@ -2,3 +2,8 @@ import musicbrainzngs
 
 if __name__ == "__main__":
     print('Hello')
+
+    musicbrainzngs.set_useragent('Song-Looker-Upper', '0.1', 'http://www.kbarnes3.com')
+
+    result = musicbrainzngs.search_recordings('"ALL OF THE LIGHTS" AND artist:"KANYE WEST" AND status:official')
+    print(result)
