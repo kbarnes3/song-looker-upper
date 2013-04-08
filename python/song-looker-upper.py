982a8466706d0c7c089559635e6f4baf196a791e
Kevin Barnes <kbarnes3@gmail.com>
2013-04-08 00:29:24 -0700

    Adding argparse and some escaping

--- python/song-looker-upper.py
+++ python/song-looker-upper.py
@@ -1,15 +1,20 @@
 import musicbrainzngs
 
 
-def lookup_song():
+def lookup_song(sql):
     musicbrainzngs.set_useragent('Song-Looker-Upper', '0.1', 'http://www.kbarnes3.com')
 
-    result = musicbrainzngs.search_recordings('"ALL OF THE LIGHTS" AND artist:"KANYE WEST" AND status:official')
+    result = musicbrainzngs.search_recordings('"BEEN CAUGHT STEALING" AND artist:"JANE\'S ADDICTION" AND status:official')
     recording = result['recording-list'][0]
     artist = recording['artist-credit'][0]['artist']['name']
-    artist = artist.replace("'", "''")
+    artist = artist.replace(u'\u2019', "'")
     title = recording['title']
-    title = title.replace("'", "''")
+    title = title.replace(u'\u2019', "'")
+
+    if sql:
+        artist = artist.replace("'", "''")
+        title = title.replace("'", "''")
+
     duration = int(int(recording['length']) / 1000)
 
     format_string = "INSERT INTO SONGS (artist, title, duration) VALUES ('{0}', '{1}', {2})"
@@ -17,4 +22,10 @@ def lookup_song():
     print(output)
 
 if __name__ == "__main__":
-    lookup_song()
+    import argparse
+
+    parser = argparse.ArgumentParser('Looks up song information from')
+    parser.add_argument('--sql', help='Escapes output for SQL commands', action='store_true')
+    args = parser.parse_args()
+
+    lookup_song(args.sql)
