ad05eef1e0e51756e1d563d7587017c02859bef0
Kevin Barnes <kbarnes3@gmail.com>
2013-04-07 23:58:13 -0700

    Results are extracted and formatted

--- python/song-looker-upper.py
+++ python/song-looker-upper.py
@@ -1,9 +1,16 @@
 import musicbrainzngs
 
 if __name__ == "__main__":
-    print('Hello')
-
     musicbrainzngs.set_useragent('Song-Looker-Upper', '0.1', 'http://www.kbarnes3.com')
 
     result = musicbrainzngs.search_recordings('"ALL OF THE LIGHTS" AND artist:"KANYE WEST" AND status:official')
-    print(result)
+    recording = result['recording-list'][0]
+    artist = recording['artist-credit'][0]['artist']['name']
+    artist = artist.replace("'", "''")
+    title = recording['title']
+    title = title.replace("'", "''")
+    duration = int(int(recording['length']) / 1000)
+
+    format_string = "INSERT INTO SONGS (artist, title, duration) VALUES ('{0}', '{1}', {2})"
+    output = format_string.format(artist, title, duration)
+    print(output)
