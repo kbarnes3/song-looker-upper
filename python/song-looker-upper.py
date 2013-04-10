373481b32d41a89c14aaf9a2c977bf42b587c58c
Kevin Barnes <kbarnes3@gmail.com>
2013-04-09 22:46:37 -0700

    Now iterates over a CSV

--- python/song-looker-upper.py
+++ python/song-looker-upper.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+import csv
+import sys
 
 import musicbrainzngs
 
@@ -17,10 +19,24 @@ def get_format(format_file_path):
         return format_string
 
 
-def lookup_song(format_string, sql):
+def lookup_songs(csv_path, format_string, sql):
+    with open(csv_path, 'rb') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            lookup_song(row[0], row[1], format_string, sql)
+
+
+def lookup_song(artist, title, format_string, sql):
     musicbrainzngs.set_useragent('Song-Looker-Upper', '0.1', 'http://www.kbarnes3.com')
 
-    result = musicbrainzngs.search_recordings('"BEEN CAUGHT STEALING" AND artist:"JANE\'S ADDICTION" AND status:official')
+    query = '"{0}" AND artist:"{1}" AND status:official'.format(title, artist)
+
+    result = musicbrainzngs.search_recordings(query)
+
+    if len(result['recording-list']) < 1:
+        print('No songs found for query ' + query, file=sys.stderr)
+        return
+
     recording = result['recording-list'][0]
     artist = recording['artist-credit'][0]['artist']['name']
     artist = artist.replace(u'\u2019', "'")
@@ -31,20 +47,21 @@ def lookup_song(format_string, sql):
         artist = artist.replace("'", "''")
         title = title.replace("'", "''")
 
-    duration = int(int(recording['length']) / 1000)
+    length = recording['release-list'][0]['medium-list'][1]['track-list'][0]['length']
+    duration = int(int(length) / 1000)
 
     output = format_string.format(artist=artist, title=title, duration=duration)
     print(output, end='')
-    print('Done')
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser('Looks up song information from')
+    parser.add_argument('CSV_FILE', help='A CSV file of artist, song pairs on each line')
     parser.add_argument('--format-file', help='Path to a file containing the output format')
     parser.add_argument('--sql', help='Escapes output for SQL commands', action='store_true')
     args = parser.parse_args()
 
     format_string = get_format(args.format_file)
 
-    lookup_song(format_string, args.sql)
+    lookup_songs(args.CSV_FILE, format_string, args.sql)
